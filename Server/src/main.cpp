f44c91ebb44d70692a65f381015691334ed41146
LazyPanda07 <semengricenko@gmail.com>
2026-02-16 02:29:25 +0500

    Add Dockerfile and custom target in cmake for building docker image

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -8,9 +8,18 @@
 
 int main(int argc, char** argv)
 {
-	std::unique_ptr<web::UDPSocket> socket = std::make_unique<web::UDPServerSocket>(8080);
+	std::unique_ptr<web::UDPSocket> socket;
 	voice::VoiceServer server;
 	web::UDPSocket::ReceiveCallback callback(server);
+
+	if (argc == 1)
+	{
+		socket = std::make_unique<web::UDPServerSocket>(8080);
+	}
+	else
+	{
+		socket = std::make_unique<web::UDPServerSocket>(static_cast<uint16_t>(std::stoi(argv[1])));
+	}
 
 	while (true)
 	{
